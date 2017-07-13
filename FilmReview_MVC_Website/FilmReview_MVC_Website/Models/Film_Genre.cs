e64b515aab109427cc3f9b711f69476172035106
Anjela Mikhaylova <anjela.mikhaylova@gmail.com>
2017-07-13 22:00:42 +0100

    Added classes to Models

---
+++ FilmReview_MVC_Website/FilmReview_MVC_Website/Models/Film_Genre.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FilmReview_MVC_Website.Models
+{
+    public class Film_Genre
+    {
+        public virtual int FilmID { get; set; }
+        public virtual int GenreID { get; set;}
+    }
+}
---
+++ FilmReview_MVC_Website/FilmReview_MVC_Website/Models/Rating.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FilmReview_MVC_Website.Models
+{
+    public class Rating
+    {
+        public virtual int RatingID { get; set; }
+        public virtual int FilmID { get; set; }
+        public virtual int UserID { get; set; }
+        public virtual int RatingScore { get; set; }
+    }
+}
