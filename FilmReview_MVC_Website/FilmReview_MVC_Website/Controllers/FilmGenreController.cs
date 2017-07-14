643ccea1e2d803baedcab2b81475eb86186bc85e
Anjela Mikhaylova <anjela.mikhaylova@gmail.com>
2017-07-14 23:51:42 +0100

    Added "Actor" Database

---
+++ FilmReview_MVC_Website/FilmReview_MVC_Website/Controllers/FilmGenreController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FilmReview_MVC_Website.Controllers
+{
+    public class FilmGenreController : Controller
+    {
+        // GET: FilmGenre
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
